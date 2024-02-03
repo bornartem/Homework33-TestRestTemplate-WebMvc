bb6da0a9decf787c589e9d01de7ee02cdc862dff
bornartem <born.artem@gmail.com>
2024-02-03 13:55:06 +0300

    SQL(references, JOIN) commit

--- scripts421.sql
+++ scripts421.sql
@@ -1,4 +1,4 @@
 alter table student add constraint age_constraint check(age<31);
 alter table student add constraint name_unique unique(name);
 alter table faculty add constraint color_name unique(color, name);
-alter table student alter age set default 20;
+alter table student alter age set default  20;
